049c50c6a8f3b7aa9f7a4b38d050a2e2f6a1bc31
ombiapp <vexosoftai@gmail.com>
2025-11-21 15:56:13 +0530

    Create listing.dart

---
+++ lib/src/models/listing.dart
@@ -0,0 +1,13 @@
+class Listing {
+  final String id;
+  final String title;
+  final String description;
+  final double price;
+
+  Listing({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.price,
+  });
+}
